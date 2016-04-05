a19013c39660e51452101d1adeb1a185367d5993
mdonov <martindonov@gmail.com>
2016-04-05 14:03:18 +0300

    Update SQL_Coursera_DU_code.sql

--- SQL_Coursera_DU_code.sql
+++ SQL_Coursera_DU_code.sql
@@ -1,4 +1,3 @@
-
 --What is the city and state of the store which had the greatest increase in average daily revenue  from November to December?
 
 SELECT sub.store, sub.MonthlyChange, str.city, str.state
@@ -7,24 +6,58 @@ FROM
 
 (SELECT
          tr.store,   
-         COUNT (distinct
-         (Case WHEN extract(MONTH from tr.saledate)=11 then tr.saledate END)) nDaysNov,
-         count(distinct
-         (Case WHEN extract(MONTH from tr.saledate)=12 then tr.saledate END)) nDaysDec,
+         COUNT (DISTINCT
+         (Case WHEN EXTRACT(MONTH from tr.saledate)=11 then tr.saledate END)) nDaysNov,
+         COUNT(DISTINCT
+         (Case WHEN EXTRACT(MONTH from tr.saledate)=12 then tr.saledate END)) nDaysDec,
 
-         SUM (case WHEN extract(MONTH from tr.saledate)=11 then tr.amt END) as NovRev,
-         SUM (case WHEN extract(MONTH from tr.saledate)=12 then tr.amt END) as DecRev,
+         SUM (case WHEN EXTRACT(MONTH from tr.saledate)=11 then tr.amt END) as NovRev,
+         SUM (case WHEN EXTRACT(MONTH from tr.saledate)=12 then tr.amt END) as DecRev,
          NovRev/nDaysNov as avgNov,
          DecRev/nDaysDec as avgDec,
          (avgDec -avgNov) as MonthlyChange
-
 FROM trnsact tr
-
-WHERE tr.stype='P' and NOT(extract(MONTH from tr.saledate)=8 and extract(year from tr.saledate)=2005)
-group by tr.store
-having nDaysDec>=20 and nDaysNov>=20) as sub
+WHERE tr.stype='P' and NOT(EXTRACT(MONTH from tr.saledate)=8 and EXTRACT(year from tr.saledate)=2005)
+GROUP BY tr.store
+HAVING nDaysDec>=20 and nDaysNov>=20) as sub
 
 JOIN strinfo as str
-on str.store=sub.store
-group by sub.store, sub.MonthlyChange, str.city, str.state
-order by sub.MonthlyChange desc
+ON str.store=sub.store
+GROUP BY sub.store, sub.MonthlyChange, str.city, str.state
+ORDER BY sub.MonthlyChange DESC;
+
+
+
+
+--Compare the average daily revenue of the store with the highest msa_income and the store with the lowest median 
+--msa_income (according to the msa_income field). In what city and state were these two stores, and which store had 
+-- a higher average daily revenue? 
+
+SELECT SUM(rads.tRevenue)/SUM(rads.ndays) as DayAverg,store_msa.msa_income Med_Income, store_msa.state, store_msa.city
+
+FROM 
+   (SELECT
+    store,
+    EXTRACT(month from saledate) as m,
+    EXTRACT(year from saledate) y,
+    COUNT(DISTINCT saledate) nDays,
+    SUM(amt) tRevenue,
+    tRevenue/nDays AVGdailyRev
+    FROM trnsact
+    WHERE stype='P' and not (m=8 and y=2005)
+    GROUP BY  store,m,y
+    HAVING ndays>20) as Rads
+
+JOIN store_msa
+ON store_msa.store= rads.store
+
+WHERE store_msa.msa_income IN ((SELECT MAX(msa_income) FROM store_msa), (SELECT MIN(msa_income)FROM store_msa))
+
+GROUP BY Med_Income, store_msa.state,store_msa.city
+
+
+
+
+
+
+
